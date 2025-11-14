17efb1b44ef327e9ec07c98ee52a3327b60540b5
lmunir1 <lmluqman15@gmail.com>
2025-11-14 14:53:54 +0000

    Deploying to gh-pages from @ PaulusLab/PaulusLab.github.io@3f622cb8edebd4d0cc3ba82435384c96f21dd6d2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "PaulusLab/PaulusLab",
+    "data-repo": "PaulusLab/PaulusLab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
